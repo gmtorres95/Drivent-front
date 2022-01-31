3f5a70f8e95ff1e77048fd03688d6b05000f1248
Pedro Hotz <pedrohotz2002@gmail.com>
2022-01-31 14:24:57 -0300

    feat: add logout button

--- src/components/Dashboard/NavigationBar/index.js
+++ src/components/Dashboard/NavigationBar/index.js
@@ -1,4 +1,4 @@
-import { Link, useLocation, useRouteMatch } from "react-router-dom";
+import { Link, useHistory, useLocation, useRouteMatch } from "react-router-dom";
 
 import styled from "styled-components";
 
@@ -9,51 +9,69 @@ import {
   FaCalendarWeek,
   FaCertificate,
 } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 
 import NavigationButton from "./NavigationButton";
+import { useContext } from "react";
+import UserContext from "../../../contexts/UserContext";
 
 export default function NavigationBar() {
+  const history = useHistory();
   const location = useLocation();
   const match = useRouteMatch();
-
+  const { setUserData } = useContext(UserContext);
   function isActive(buttonPath) {
     return location.pathname === buttonPath;
   }
 
+  function logout() {
+    setUserData("");
+    localStorage.clear();
+    history.push("/sign-in");
+  }
+
   return (
     <Container>
-      <Link to={`${match.path}/subscription`}>
-        <NavigationButton active={isActive(`${match.path}/subscription`)}>
-          <FaFileContract />
-          <span>Inscrição</span>
-        </NavigationButton>
-      </Link>
+      <div>
+        <Link to={`${match.path}/subscription`}>
+          <NavigationButton active={isActive(`${match.path}/subscription`)}>
+            <FaFileContract />
+            <span>Inscrição</span>
+          </NavigationButton>
+        </Link>
 
-      <Link to={`${match.path}/payment`}>
-        <NavigationButton active={isActive(`${match.path}/payment`)}>
-          <FaMoneyBill />
-          <span>Pagamento</span>
-        </NavigationButton>
-      </Link>
+        <Link to={`${match.path}/payment`}>
+          <NavigationButton active={isActive(`${match.path}/payment`)}>
+            <FaMoneyBill />
+            <span>Pagamento</span>
+          </NavigationButton>
+        </Link>
 
-      <Link to={`${match.path}/hotel`}>
-        <NavigationButton active={isActive(`${match.path}/hotel`)}>
-          <FaBed />
-          <span>Hotel</span>
-        </NavigationButton>
-      </Link>
+        <Link to={`${match.path}/hotel`}>
+          <NavigationButton active={isActive(`${match.path}/hotel`)}>
+            <FaBed />
+            <span>Hotel</span>
+          </NavigationButton>
+        </Link>
 
-      <Link to={`${match.path}/activities`}>
-        <NavigationButton active={isActive(`${match.path}/activities`)}>
-          <FaCalendarWeek />
-          <span>Atividades</span>
-        </NavigationButton>
-      </Link>
+        <Link to={`${match.path}/activities`}>
+          <NavigationButton active={isActive(`${match.path}/activities`)}>
+            <FaCalendarWeek />
+            <span>Atividades</span>
+          </NavigationButton>
+        </Link>
 
-      <Link to={`${match.path}/certificate`}>
-        <NavigationButton active={isActive(`${match.path}/certificate`)}>
-          <FaCertificate />
-          <span>Certificado</span>
+        <Link to={`${match.path}/certificate`}>
+          <NavigationButton active={isActive(`${match.path}/certificate`)}>
+            <FaCertificate />
+            <span>Certificado</span>
+          </NavigationButton>
+        </Link>
+      </div>
+      <Link to={`${match.path}/sign-in`} onClick={logout} style={{ textDecoration: "none" }}>
+        <NavigationButton>
+          <FiLogOut />
+          <span>Logout</span>
         </NavigationButton>
       </Link>
     </Container>
@@ -67,10 +85,11 @@ const Container = styled.div`
   box-shadow: 2px 0 10px 0 rgba(0,0,0,0.1);
   width: 100px;
   flex-shrink: 0;
-  justify-content: flex-start;
-
-  > a {
+  justify-content: space-between;
+  div {
+    > a {
     text-decoration: none;
+    }
   }
 
   @media (max-width: 600px) {
@@ -79,3 +98,4 @@ const Container = styled.div`
     flex-direction: row;
   }
 `;
+
