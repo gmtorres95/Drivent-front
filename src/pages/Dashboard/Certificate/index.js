10a8d45da0b4efd4fcbabd6b8d1b05b17544f8fa
Pedro Hotz <pedrohotz2002@gmail.com>
2022-02-03 01:38:15 -0300

    feat: add download pdf function

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,3 +1,30 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import styled from "styled-components";
+
 export default function Certificate() {
-  return "Certificado: Em breve!";
+  function downloadPDF() {
+    const element = document.getElementById("pdf");
+    html2canvas(element).then((canvas) => {
+      const img = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(img, "JPEG", 0, 0);
+      pdf.save("myCertificate.pdf");
+    });
+  }
+
+  return (
+    <><Container id="pdf">
+      <div>Teste</div>
+    </Container>
+    <StyledButton onClick={downloadPDF}>Baixar PDF</StyledButton></>
+  );
 }
+
+const Container = styled.div`
+width: 100%;
+`;
+
+const StyledButton = styled.button`
+     width: 100px;
+`;
